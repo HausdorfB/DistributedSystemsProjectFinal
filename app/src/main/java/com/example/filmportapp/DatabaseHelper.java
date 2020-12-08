dcc97118b41dee1644798f5b1b3b7041d7ee0728
Hausgod <43155588+HausdorfB@users.noreply.github.com>
2020-12-07 23:48:09 -0500

    Update DatabaseHelper.java

--- app/src/main/java/com/example/filmportapp/DatabaseHelper.java
+++ app/src/main/java/com/example/filmportapp/DatabaseHelper.java
@@ -12,12 +12,7 @@ import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "user";
-
-    private static final String COL_1 = "Email";
     private static final String COL_2 = "Username";
-    private static final String COL_3 = "Password";
-    private static final String COL_4 = "Adventures";
-    private static final String COL_5 = "Points";
     private SQLiteDatabase db;
 
     public DatabaseHelper(Context context) {
@@ -27,16 +22,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         this.db = db;
-
-        String createTableUsers = "CREATE TABLE " + TABLE_NAME + "(" +
-                COL_1 + " Text NOT NULL," +
-                COL_2 + " Text NOT NULL," +
-                COL_3 + " Text NOT NULL," +
-                COL_4 + " Integer NOT NULL," +
-                COL_5 + " Integer NOT NULL)" + ";";
-
-        //Log.d("DBText","createTable: "+createTableUsers);
-        db.execSQL(createTableUsers);
 
         //Create table for questionnaire results
         final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
@@ -71,7 +56,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         addQuestion(q5);
     }
 
-    private void addQuestion(Question question){
+    private void addQuestion(Question question){ // Used to initialize the questions being used
         ContentValues cv = new ContentValues();
         cv.put(QuizInfo.QuestionTable.COLUMN_QUESTION, question.getQuestion());
         cv.put(QuizInfo.QuestionTable.COLUMN_OPTION1, question.getOption1());
@@ -85,9 +70,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<Question> questionList = new ArrayList<>();
         db = getReadableDatabase();
         Cursor c = db.rawQuery("SELECT * FROM " + QuizInfo.QuestionTable.TABLE_NAME, null);
-
+        
         if (c.moveToFirst()) {
-            do {
+            do { // generic method of dynamically setting questions from database when method is called
                 Question question = new Question();
                 question.setQuestion(c.getString(c.getColumnIndex(QuizInfo.QuestionTable.COLUMN_QUESTION)));
                 question.setOption1(c.getString(c.getColumnIndex(QuizInfo.QuestionTable.COLUMN_OPTION1)));
@@ -102,60 +87,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return questionList;
     }
 
-    public boolean addRecord (String email,String username,String password, int adventures, int points){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1, email);
-        contentValues.put(COL_2, username);
-        contentValues.put(COL_3, password);
-        contentValues.put(COL_4, adventures);
-        contentValues.put(COL_5, points);
-        long ins = db.insert(TABLE_NAME,null,contentValues);
-        if (ins==0) return false;
-        else return true;
-    }
-
-    public boolean checkUsers (String username, String password){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = "select * from " + TABLE_NAME
-                + " where " + COL_2 + " = " + "'" +username + "'" +
-                " and " +  COL_3 + " = " + "'" +password+"'" +" ; ";
-
-        Log.d("query",query);
-        Cursor c = db.rawQuery(query,null);
-        int rowsCount= c.getCount();
-        c.close();
-        if (rowsCount>0) return true;
-        else return false;
-    }
-
-
-    public boolean checkUsername(String username) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = "select * from " + TABLE_NAME
-                + " where " + COL_2 + " = " + "'" +username + "'" +" ; ";
-
-        Log.d("query",query);
-        Cursor c = db.rawQuery(query,null);
-        int rowsCount= c.getCount();
-        c.close();
-        if (rowsCount>0) return true;
-        else return false;
-    }
-
-    public void addPoints(String username, int adventures, int points) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + COL_2 + "=" + "\"" + username + "\"", null);
-        cursor.moveToFirst();
-        adventures += cursor.getInt(cursor.getColumnIndex(COL_4)); // add previously existing adventures to the update
-        points += cursor.getInt(cursor.getColumnIndex(COL_5)); // same with points
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2, username);
-        contentValues.put(COL_4, adventures);
-        contentValues.put(COL_5, points);
-        db.update(TABLE_NAME, contentValues, " Username = " + "\"" + username + "\"", null); // update the table
-    }
-
+    //for displaying database contents
     public Cursor viewData() {
         SQLiteDatabase db = this.getReadableDatabase();
         String query = "SELECT * FROM " + TABLE_NAME;
